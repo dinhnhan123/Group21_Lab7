bff4a62d6185fc4a1d1a78c79d4d6b5881884a70
dinhnhan123 <20130344@st.hcmuaf.edu.vn>
2025-12-07 20:03:42 +0700

    Sync test tool with team setup for Lab 7

---
+++ 20130344_DinhThanhNhan_Lab7/LoginTestSuite/LoginWrongPassword.java
@@ -0,0 +1,84 @@
+package com.example.LoginTestSuite;
+
+import java.util.regex.Pattern;
+import java.util.concurrent.TimeUnit;
+import org.junit.*;
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+import org.apache.commons.io.FileUtils;
+import java.io.File;
+
+public class LoginWrongPassword {
+  private WebDriver driver;
+  private String baseUrl;
+  private boolean acceptNextAlert = true;
+  private StringBuffer verificationErrors = new StringBuffer();
+  JavascriptExecutor js;
+  @Before
+  public void setUp() throws Exception {
+    System.setProperty("webdriver.chrome.driver", "");
+    driver = new ChromeDriver();
+    baseUrl = "https://www.google.com/";
+    driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(60));
+    js = (JavascriptExecutor) driver;
+  }
+
+  @Test
+  public void testLoginWrongPassword() throws Exception {
+    driver.get("https://mwc.com.vn/login");
+    driver.findElement(By.id("UserName")).click();
+    driver.findElement(By.id("UserName")).clear();
+    driver.findElement(By.id("UserName")).sendKeys("dinhnhan12345");
+    driver.findElement(By.id("Password")).click();
+    driver.findElement(By.id("Password")).clear();
+    driver.findElement(By.id("Password")).sendKeys("000000");
+    driver.findElement(By.xpath("//input[@value='Đăng nhập']")).click();
+    driver.findElement(By.xpath("//img[@alt='MWC']")).click();
+    driver.get("https://mwc.com.vn/");
+  }
+
+  @After
+  public void tearDown() throws Exception {
+    driver.quit();
+    String verificationErrorString = verificationErrors.toString();
+    if (!"".equals(verificationErrorString)) {
+      fail(verificationErrorString);
+    }
+  }
+
+  private boolean isElementPresent(By by) {
+    try {
+      driver.findElement(by);
+      return true;
+    } catch (NoSuchElementException e) {
+      return false;
+    }
+  }
+
+  private boolean isAlertPresent() {
+    try {
+      driver.switchTo().alert();
+      return true;
+    } catch (NoAlertPresentException e) {
+      return false;
+    }
+  }
+
+  private String closeAlertAndGetItsText() {
+    try {
+      Alert alert = driver.switchTo().alert();
+      String alertText = alert.getText();
+      if (acceptNextAlert) {
+        alert.accept();
+      } else {
+        alert.dismiss();
+      }
+      return alertText;
+    } finally {
+      acceptNextAlert = true;
+    }
+  }
+}
